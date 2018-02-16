43fa886334309b453c67c6cf4ddaf70f3b830e34
Frederik Aarup Lauridsen <frederik.lauridsen@gmail.com>
2018-02-16 14:41:36 +0100

    gemmer numre for hver server

--- routes.py
+++ routes.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 from functools import wraps
 import werkzeug.contrib
+from collections import defaultdict
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+numbers = defaultdict(list)
+round2 = defaultdict(list)
+
+
 @app.route("/")
 def home():
-    return 'Hello, World!'
+    return string(numbers)
 
 @app.route("/bp/server<int:name>", methods=["POST"])
-def server1(name):
-    name1 = request.args.get("name")
-    value1 = request.args.get("value")
-
+def server1(id):
     name1 = request.form.get("name")
     value1 = request.form.get("value")
+    numbers[id].append((value1, name1))
     print(request.method)
     print(request.json)
-
+    
     for i in request.json:
         print(i, request.json.get(i))
         
