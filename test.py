89efc8dada70f885bfee840ae6dd3be4a24b8b73
andershjensen <anders-h-jensen@hotmail.com>
2018-02-23 10:42:35 +0100

    Added method for creating secrets for multiplication and tests for addition and multiplication relying only on the way secrets are generated

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import requests
 import unittest
 import time
 
-
 p = 4000001
 baseurl1 = "http://127.0.0.1:5000/"
 baseurl2 = "http://127.0.0.1:5001/"
@@ -18,14 +17,25 @@ official_server4 = "https://server4.cryptovoting.dk/"
 n_servers = [baseurl1, baseurl2, official_server, official_server1, official_server2, official_server3, official_server4]
 
 
-# class test_arithmetics(unittest.TestCase):
-#
-#     def test_addition(self):
-#         x = util.create_secret(22, 100, 3)
-#         y = util.create_secret(28, 100, 3)
-#         res = sum(x) + sum(y)
-#         self.assertEqual(int(res), 50)
-#
+
+class test_arithmetics(unittest.TestCase):
+
+    def test_addition(self):
+        secrets_1 = util.create_addition_secret(10, 2, baseurl1 + 'server')
+        secrets_2 = util.create_addition_secret(15, 2, baseurl2 + 'server')
+        res = sum(secrets_1) + sum(secrets_2)
+        self.assertEqual(25, res)
+
+ ##TODO: make partitioning of numbers for multiplication (split the number into amount _of_servers parts)
+    def test_multiplication(self):
+        secrets_1 = util.create_multiplication_secret(5, 2)
+        secrets_2 = util.create_multiplication_secret(5, 2)
+        res = 0
+        for x in secrets_1:
+            for y in secrets_2:
+                res += x * y
+        self.assertEqual(25, res)
+
 
 class test_communication(unittest.TestCase):
 
@@ -41,16 +51,9 @@ class test_communication(unittest.TestCase):
         util.create_and_post_secret_to_servers(12, "x", servers)
         util.create_and_post_secret_to_servers(18, "x", servers)
 
-
-
         total = util.getTotal([baseurl1, baseurl2, baseurl3])
-        # num1 = requests.get(baseurl1 + "total").text
-        # num2 = requests.get(baseurl2 + "total").text
-        # p = util.get_prime(baseurl1 + "server" + '/prime')
+
         self.assertEqual(50, total)
-        # print(num1, "+", num2, "=", int(num1) + int(num2))
-        # print("database1:", requests.get(baseurl1 + "databases").text)
-        # print("database2:", requests.get(baseurl2 + "databases").text)
 
     def test_n_official_servers(self):
         servers = [server + "server" for server in n_servers]
@@ -63,9 +66,6 @@ class test_communication(unittest.TestCase):
         time.sleep(0.1)
 
         total = util.getTotal(n_servers)
-
-        # for server in n_servers:
-        #     total += int(requests.get(server + "total").text)
 
         self.assertEqual(50, total % util.get_prime(servers[0]))
 
--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import random
 
 baseurl1 = "http://127.0.0.1:5000/"
@@ -13,13 +12,24 @@ official_url = "https://cryptovoting.dk/"
 work_url = baseurl1
 
 
-def create_secret(x: int, n: int, server_url: str):
+def create_addition_secret(x: int, n: int, server_url: str):
     p = get_prime(server_url)
     rng = random.Random()
     res = []
     for i in range(n - 1):
         res.append(rng.randrange(0, p - 1, 1))
     res.append(x - sum(res))
+    return res
+
+def create_multiplication_secret(x: int, n: int):
+    remaining = x
+    rng = random.Random()
+    res = []
+    for i in range(n-1):
+        secret = rng.randrange(0, remaining)
+        res.append(secret)
+        remaining -= secret
+    res.append(remaining)
     return res
 
 
@@ -45,48 +55,6 @@ def getTotal(urls: list):
     return total % int(get_prime(urls[0] + 'server'))
 
 
-# Sieve of Eratosthenes
-# Code by David Eppstein, UC Irvine, 28 Feb 2002
-# http://code.activestate.com/recipes/117119/
-
-def gen_primes():
-    """ Generate an infinite sequence of prime numbers.
-    """
-    # Maps composites to primes witnessing their compositeness.
-    # This is memory efficient, as the sieve is not "run forward"
-    # indefinitely, but only as long as required by the current
-    # number being tested.
-    #
-    D = {}
-
-    # The running integer that's checked for primeness
-    q = 2
-
-    while True:
-        if q not in D:
-            # q is a new prime.
-            # Yield it and mark its first multiple that isn't
-            # already marked in previous iterations
-            #
-            yield q
-            D[q * q] = [q]
-        else:
-            # q is composite. D[q] is the list of primes that
-            # divide it. Since we've reached q, we no longer
-            # need it in the map, but we'll mark the next
-            # multiples of its witnesses to prepare for larger
-            # numbers
-            #
-            for p in D[q]:
-                D.setdefault(p + q, []).append(p)
-            del D[q]
-
-        q += 1
-
-
-primes = gen_primes()
-
-
 def get_prime(url: str):
     prime = requests.get(url + '/prime').text
     return int(prime)
@@ -101,7 +69,7 @@ def post_secret_to_server(name: list, value: list, url: str):
 
 
 def create_and_post_secret_to_servers(x: int, name: str, servers: list):
-    secrets = create_secret(x, len(servers), servers[0])
+    secrets = create_addition_secret(x, len(servers), servers[0])
     names = ["r" + str(i) for i in range(len(secrets))]
     for num, server_url in enumerate(servers):
         # print(requests.post(server_url, data=dict(name=name, value=secrets[num])))
@@ -110,11 +78,3 @@ def create_and_post_secret_to_servers(x: int, name: str, servers: list):
         names_c = names.copy()
         del names_c[num]
         print(post_secret_to_server(names_c, secrets_c, server_url))
-
-# post_url(url=baseurl1 + "server", data={"name": ["a", "b", "c"], "value": [1,2,3]})
-
-
-# for num in primes:
-#     if num >= 4000001:
-#         print(num)
-#         break
