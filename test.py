634926892b59d862e2ce48bdb44c6f5cee8b6a33
Christian Hundahl <christian@hundahl.eu>
2018-06-11 14:59:22 +0200

    Made zero one check part of check votes

--- test.py
+++ test.py
@@ -246,8 +246,8 @@ class TestCommunication(unittest.TestCase):
         illegal_vote = np.array([[3,-2],[-2,3]])
         illegal_vote_partitions = util.partition_and_secret_share_vote(illegal_vote, local_servers)
         client_util.postvote("ic3", illegal_vote_partitions, local_servers)
-        illegal_vote = np.array([[2, -1], [-1, 2]])
         client_util.send_vote([2,1], 'c2', local_servers)
+        illegal_vote = np.array([[2, -1], [-1, 2]])
         illegal_vote_partitions = util.partition_and_secret_share_vote(illegal_vote, local_servers)
         client_util.postvote("ic4", illegal_vote_partitions, local_servers)
         for server in local_servers:
